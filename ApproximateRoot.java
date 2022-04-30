59b99be44e06e6d5281ce5ec01b04806c4a2d86c
BotchedPrograms <103963170+BotchedPrograms@users.noreply.github.com>
2022-04-30 14:38:55 -0400

    Create ApproximateRoot.java

---
+++ ApproximateRoot.java
@@ -0,0 +1,39 @@
+public class ApproximateRoot {
+  private static final double num = 69;
+  
+  public static double abs(double num) {
+    if (num < 0) {
+      return -num;
+    }
+    return num;
+  }
+
+  // Gets square root without Math library
+  public static void main(String[] args) {
+    int intPart = -1;
+    for (int i = 0; i < num; i++) {
+      if (i * i >= num) {
+        intPart = i-1;
+        break;
+      }
+    }
+    double sqrt = intPart;
+    double n1 = 0;
+    double d1 = 1;
+    double n2 = 1;
+    double d2 = 1;
+    double newNum;
+    while (abs(sqrt*sqrt - num) > 0.0000000001) {
+      newNum = (n1+n2)/(d1+d2);
+      sqrt = intPart + newNum;
+      if (sqrt*sqrt < num) {
+        n1 = n1+n2;
+        d1 = d1+d2;
+      } else if (sqrt*sqrt > num) {
+        n2 = n1+n2;
+        d2 = d1+d2;
+      }
+    }
+    return sqrt;
+  }
+}
